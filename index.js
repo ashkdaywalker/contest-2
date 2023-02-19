2289e4cfec77a831b2d5e38f8836851a4f89dced
ashkdaywalker <116715716+ashkdaywalker@users.noreply.github.com>
2023-02-19 03:58:31 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+console.log("fetching Data");
+
+"fetch('https://free-food-menus-api-production.up.railway.app/burgers')
+.then(res => res.json())
+.then(console.log);"
+let myDiv = document.getElementId("results");
+myDiv.innerHtml = <p><strong>${Data.id} - ${data.author}</strong>${data.quote}</p>
+console.log(myData);
